478d18159a07bfe4d52ef58d9323665b1add7213
fengkuangdeshitou <409744573@qq.com>
2025-11-21 18:36:24 +0800

    移除所有测试相关文件和代码

--- gui/server/test-import.js
+++
@@ -1,5 +0,0 @@
-import Dypnsapi, * as $Dypnsapi from '@alicloud/dypnsapi20170525';
-import OpenApi, * as $OpenApi from '@alicloud/openapi-client';
-import Util from '@alicloud/tea-util';
-
-console.log('SDK导入成功');
